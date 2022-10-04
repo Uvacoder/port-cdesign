c46b4ca63187ae2cc34feefa87fda8494dfab640
Charles026 <charles0260@outlook.com>
2022-10-04 18:26:23 +0800

    tailwind css install

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../component/navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,24 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>     
-      <div>
-        Hello, Charles!
-      </div>
-      </main>
+      <h1 className="text-3xl font-bold underline">
+      Hello Charles!
+    </h1>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+
     </div>
   )
 }
